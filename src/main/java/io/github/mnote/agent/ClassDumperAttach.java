7c3e79b1339b2a1ec0cb427bc4d07714a8ef8d7e
maoling <maoling@meituan.com>
2018-11-26 10:19:39 +0800

    find PID

--- src/main/java/io/github/mnote/agent/ClassDumperAttach.java
+++ src/main/java/io/github/mnote/agent/ClassDumperAttach.java
@@ -1,8 +1,10 @@
 package io.github.mnote.agent;
 
 import com.sun.tools.attach.VirtualMachine;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.lang.management.ManagementFactory;
 
 /**
  * Simple attach-on-demand client tool that loads the given agent into the given Java process.
@@ -10,19 +12,39 @@ import java.io.File;
 public class ClassDumperAttach {
 
     public static void main(String[] args) throws Exception {
+        attach();
+    }
 
-        String pid = "3687";
+    public static void attach() throws Exception {
+
+        String pid = pid();
+
+        System.out.println("pid: " + pid);
 
         File file = new File("target/aspectj-1.0.0.jar");
         String agent = file.getAbsolutePath();
 
-        System.out.println(agent);
+        System.out.println("agent: " + agent);
 
         // JVM is identified by process id (pid).
         VirtualMachine vm = VirtualMachine.attach(pid);
 
+        File tmp = new File("./tmp");
+        if (tmp.isDirectory()) {
+            System.out.println("removing: " + tmp.getAbsolutePath());
+            FileUtils.deleteDirectory(tmp);
+        }
+
         // load a specified agent onto the JVM
         vm.loadAgent(agent, "dumpDir=./tmp,classes=io.github.*");
+
     }
+
+    public static String pid() {
+        String jvmName = ManagementFactory.getRuntimeMXBean().getName();
+        System.out.println("jvmName: " + jvmName);
+        return jvmName.split("@")[0];
+    }
+
 
 }
